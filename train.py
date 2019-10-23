ddb047c0588cbf2da30b4244e7ac3209ef552b2f
zaberko <zaberchann@gmail.com>
2019-10-23 14:25:03 +0800

    move normal_cell_config to bottom of train.py for convenience

--- train.py
+++ train.py
@@ -16,20 +16,19 @@ import argparse
 import json
 
 
+def train(model, trainloader, testloader, device):
 
-def train(model, trainloader, testloader,device):
-    
     optimizer = torch.optim.Adam(model.parameters(), lr=train_config['lr'])
     loss_func = torch.nn.CrossEntropyLoss()
-    
+
     running_loss = 0.0
-    total_step=0
+    total_step = 0
     for epoch in range(train_config['epoch']):
         start_time = time.time()
         model.train()
-        
+
         for step, data in enumerate(trainloader, 0):
-            data=tuple(t.to(device) for t in data)
+            data = tuple(t.to(device) for t in data)
             images, labels = data
             output = model(images)
             loss = loss_func(output, labels)
@@ -38,25 +37,27 @@ def train(model, trainloader, testloader,device):
             optimizer.step()
 
             running_loss += loss.item()
-            total_step+=1
+            total_step += 1
             if step % 10 == 9:
-                print('epoch: {0}, iter:{1} loss:{2:.4f} avg_loss:{3:.4f}'.format(epoch, step, loss.item(),running_loss / total_step))
-                
+                print('epoch: {0}, iter:{1} loss:{2:.4f} avg_loss:{3:.4f}'.format(
+                    epoch, step, loss.item(), running_loss / total_step))
+
         print('epoch{} test: '.format(epoch), end="")
-        evaluate(model, testloader,device)
+        evaluate(model, testloader, device)
         # print('epoch{} train: '.format(epoch), end="")
         # evaluate(model, trainloader)
-        print('epoch {} finished, cost {:.3f} sec'.format(epoch, time.time() - start_time))
+        print('epoch {} finished, cost {:.3f} sec'.format(
+            epoch, time.time() - start_time))
         print('=======================\n\n\n')
 
 
-def evaluate(model: torch.nn.Module, testloader,device):
+def evaluate(model: torch.nn.Module, testloader, device):
     correct = 0
     total = 0
     model.eval()
     with torch.no_grad():
         for data in testloader:
-            data=tuple(t.to(device) for t in data)
+            data = tuple(t.to(device) for t in data)
             images, labels = data
 
             output = model(images)
@@ -68,19 +69,20 @@ def evaluate(model: torch.nn.Module, testloader,device):
             #     break
 
     acc = correct / total * 100
-    print('Accuracy of the network on the 10000 test images: {:.3f}'.format(acc))
+    print(
+        'Accuracy of the network on the 10000 test images: {:.3f}'.format(acc))
 
     return acc
 
 
-def load_dataset(path: str,batch_size:int):
+def load_dataset(path: str, batch_size: int):
     mean = [0.49139968, 0.48215827, 0.44653124]
     std = [0.24703233, 0.24348505, 0.26158768]
-    transf=[
-            transforms.RandomCrop(32, padding=4),
-            transforms.RandomHorizontalFlip()
+    transf = [
+        transforms.RandomCrop(32, padding=4),
+        transforms.RandomHorizontalFlip()
     ]
-    normalize =[
+    normalize = [
         transforms.ToTensor(),
         transforms.Normalize(mean, std)
     ]
@@ -108,30 +110,24 @@ def save_model():
 def load_model():
     pass
 
+
 def main():
     random.seed(train_config['seed'])
     np.random.seed(train_config['seed'])
     torch.manual_seed(train_config['seed'])
     torch.cuda.manual_seed_all(train_config['seed'])
 
-    batch_size=train_config['batch_size']
-    
-    trainloader, testloader, classes = load_dataset(train_config['data_path'],batch_size)
+    batch_size = train_config['batch_size']
 
-    config_list = {
-        2: [(0, 2)],
-        3: [(2, 4)],
-        4: [(3, 4)],
-        5: [(1, 6), (2,6)],
-        6: [(0, 5)],
-        7: [(4, 1), (5, 1), (6,1)]
-    }
-    cell_config_list = {'normal_cell': config_list}
+    trainloader, testloader, classes = load_dataset(
+        train_config['data_path'], batch_size)
+
+    cell_config_list = {'normal_cell': normal_cell_config}
 
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     n_gpu = torch.cuda.device_count()
 
-    model=NASNetCIFAR(
+    model = NASNetCIFAR(
         cell_config_list,
         nasnet_config['num_stem_channels'],
         nasnet_config['cell_base_channels'],
@@ -144,34 +140,34 @@ def main():
         model.half()
 
     model.to(device)
-    if n_gpu>1:
-        model=nn.DataParallel(model)
+    if n_gpu > 1:
+        model = nn.DataParallel(model)
 
-    train(model, trainloader,testloader,device)
+    train(model, trainloader, testloader, device)
 
     # evaluate(model, testloader)
 
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--config_path',default='config.json',type=str,help="config file path")
-    parser.add_argument('--do_train',action='store_true')
-    parser.add_argument('--do_eval',action='store_true')
-    args= parser.parse_args()
-    config_path=args.config_path
+    parser.add_argument('--config_path', default='config.json',
+                        type=str, help="config file path")
+    parser.add_argument('--do_train', action='store_true')
+    parser.add_argument('--do_eval', action='store_true')
+    args = parser.parse_args()
+    config_path = args.config_path
 
-    with open(config_path,mode='r',encoding='utf-8') as f:
-        config=json.load(f)
+    with open(config_path, mode='r', encoding='utf-8') as f:
+        config = json.load(f)
 
-
-    train_config=config['train_config']
-    nasnet_config=config['nasnet_config']
-
+    train_config = config['train_config']
+    nasnet_config = config['nasnet_config']
+    normal_cell_config = {
+        2: [(0, 2)],
+        3: [(2, 4)],
+        4: [(3, 4)],
+        5: [(1, 6), (2, 6)],
+        6: [(0, 5)],
+        7: [(4, 1), (5, 1), (6, 1)]
+    }
     main()
-
-
-    
-
-
-
-
-  
